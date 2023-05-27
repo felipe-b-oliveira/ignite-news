ee4f2457a9b96a31c6a1be955e36ead2e108570a
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-05-27 01:26:41 -0300

    feat: adiciona sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from "../styles/home.module.scss";
+
 import React from "react";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className={styles.title}>
+      Hello<span>World</span>
+    </h1>
   )
 }
