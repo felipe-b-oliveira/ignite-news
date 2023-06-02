22184a5d6448e360249f61d36db94c45bbb723bd
Felipe Barbosa <felipe.barbosa@calindra.com.br>
2023-06-01 21:17:39 -0300

    feat: add globalstyle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Head from 'next/head';
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>World</span>
+      <h1>
+        Hello World
       </h1>
     </>
   )
